1efd52ade895ba4b4558fb22540d4f837af6e671
mnkkhan <mnkkhan77@gmail.com>
2025-06-30 19:21:51 +0530

    fix: reduced chunk size on build

--- job-portal-frontend/vite.config.js
+++ job-portal-frontend/vite.config.js
@@ -4,8 +4,17 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 600, // optional silence
     rollupOptions: {
-      external: ["clsx"],
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("@mui")) return "mui"; // ① all Material UI
+            if (id.includes("react")) return "react"; // ② react / react-dom
+            return "vendor"; // ③ the rest
+          }
+        },
+      },
     },
   },
 });
